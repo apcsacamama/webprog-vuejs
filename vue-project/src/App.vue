b48b1817b434134e0f55a043654d4c3ae20222c3
apcsacamama <sacamama@student.apc.edu.ph>
2025-01-27 22:24:01 +0800

    Personal Profile added

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Food</h1>
+  <perosnal-profile/> <br>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+  <header><h1>My Personal Profile Webpage</h1></header>
+  <main>
+  <section class="main-content">
+    <h2>About Me</h2>
+    <p>Hello! My name is Shanteiy A. Camama, but you can also call me shan.
+        I am currently in 2nd year of college and here are some of the things about me.</p>
+
+    <h2>Education / Achievements</h2>
+        <ul>
+            <li>2022-2023 : 1st Place in MathemArtists Art Making Competition, Grade 12 with honor </li>
+            <li>2023-2024 : 1st year Honor student</li>
+        </ul>
+
+    <h2>Course</h2>
+    <p>Bachelor of Science in Computer Science Specializing in Software Systems.</p>
+
+    <h2>IT Experience</h2>
+    <p>I have worked on various IT projects including:</p>
+    <ul>
+        <li>Front-end and Back-end programming</li>
+        <li>Database management</li>
+    </ul>
+
+    <h2>Hobbies & Interests</h2>
+    <p>Here are some of my hobbies:</p>
+    <ul>
+        <li>Reading manhwas and manga</li>
+        <li>Playing instruments like the ukulele and keyboard</li>
+        <li>Volleyball</li>
+        <li>Traveling </li>
+    </ul>
+
+    <h2>Goals In Life / Dream</h2>
+    <p>My dream is to become a successful software developer like my sister and be able to travel around the world once I'm financially stable.</p>
+
+    <h2>Picture Gallery</h2>
+    <p>Here are some pictures that represent me:</p>
+    <img src="https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg" alt="Placeholder Image 1" width="150">
+    <img src="https://i.pinimg.com/736x/ac/e3/16/ace3163f3edb4b0c311ea0e56bb9d5d1.jpg" alt="Placeholder Image 2" width="150">
+    <img src="https://i.pinimg.com/736x/bb/e9/85/bbe985b0966c8cd51df295e6389e080c.jpg" alt="Placeholder Image 3" width="150">
+    <img src="https://i.pinimg.com/736x/06/ad/da/06adda610b5c8af46f61afa7779760a0.jpg" alt="Placeholder Image 4" width="150">
+    <footer>Asia Pacific College</footer>
+</template>
+
+<script></script>
+
+<style></style>

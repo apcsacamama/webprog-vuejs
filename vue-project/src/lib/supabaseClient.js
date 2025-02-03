c550526e6308257df530066bfd47f9c222dfd314
apcsacamama <sacamama@student.apc.edu.ph>
2025-02-03 23:35:01 +0800

    commit

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
-import { createClient } from '@supabase/ supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient('https://wrvagtvruzlxyezajhqx.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndydmFndHZydXpseHllemFqaHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNDY4MzAsImV4cCI6MjA1MzgyMjgzMH0.XY3Z_AQGs0tUTehXd6WOZByvJ6bB-g6fsr43Vkp5dao')

9333045b621e6386059cb8fdc5ecb0feac887fcb
apcsacamama <sacamama@student.apc.edu.ph>
2025-02-03 15:48:24 +0000

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,26 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+</script>
+
+
 <template>
+
+<ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+
   <h1>Food</h1>
   <personal-profile/> <br>
   <food-item/> <br>
@@ -6,7 +28,5 @@
   <food-item/> <br>
   <food-item2/> <br>
 </template>
-
-<script></script>
 
 <style></style>

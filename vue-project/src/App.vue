9c5b002548ae3cd9361fe1d5cd6aaa040d872cd9
apcsacamama <sacamama@student.apc.edu.ph>
2025-02-04 11:59:46 +0800

    please work

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,21 +12,20 @@
   onMounted(() => {
     getCountries()
   })
-</script>
-
+  </script>
 
 <template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
 
-<ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-</ul>
-
-  <h1>Food</h1>
+  
+   <h1>Food</h1>
   <personal-profile/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
 </template>
 
 <style></style>
